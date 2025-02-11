dee7b2df4604984dcb70625cc44fc5d64c7ab7ef
espressotoexpressions <keannevillar@gmail.com>
2025-02-10 16:53:22 -0800

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,156 @@
+import {
+  addExpense,
+  deleteExpense,
+  updateExpense,
+  retrieveExpenses,
+  addIncome,
+  deleteIncome,
+  retrieveIncome,
+  getBudgetLeft,
+} from "./localStorage.js";
+let expenseItemSection = document.getElementById("expenseItemSection");
+let incomeItemSection = document.getElementById("incomeItemSection");
+let remainingBudget = document.getElementById("remainingBudget");
+let expenseNameTxt = document.getElementById("expenseName");
+let expenseAmtTxt = document.getElementById("expenseAmt");
+let addExpenseBtn = document.getElementById("addExpenseBtn");
+let incomeAmtTxt = document.getElementById("incomeAmt");
+let addIncomeBtn = document.getElementById("addIncomeBtn");
+let expenseErrorMsg = document.getElementById("expenseErrorMsg");
+let incomeErrorMsg=document.getElementById("incomeErrorMsg");
+
+//to populate expense list
+function populateExpenseSection() {
+  expenseItemSection.innerText = ""; //resets the section to avoid duplicate entries
+  let expenseData = retrieveExpenses();
+
+  expenseData.map((expense) => {
+    console.log("ID" + expense.id);
+    console.log("AMT" + expense.amount);
+
+    const expenseSectionItem = document.createElement("div"); // favorite item  create
+    expenseSectionItem.classList.add(
+      "flex",
+      "items-center",
+      "p-2",
+      "text-gray-900",
+      "rounded-lg",
+      "dark:text-white",
+      "hover:bg-amber-100",
+      "dark:hover:bg-amber-700",
+      "group"
+    );
+
+    // Create a <span> for the text to separate it from the icon
+    const expenseText = document.createElement("input");
+    expenseText.value = `${expense.name}`;
+    expenseSectionItem.appendChild(expenseText); // Add the text to the <li>
+
+    //create another <span> for the amount
+    const expenseAmount = document.createElement("input");
+    expenseAmount.value = `${expense.amount}`;
+    expenseSectionItem.appendChild(expenseAmount); // Add the amount to the <li>
+
+    // create an updatebutton
+    const updateExpenseBtn = document.createElement("button");
+    updateExpenseBtn.innerText = "Update";
+    expenseSectionItem.appendChild(updateExpenseBtn);
+
+    updateExpenseBtn.addEventListener('click',()=>{
+        updateExpense( expense.id, expenseText.value,expenseAmount.value);
+        populateExpenseSection();
+        displayRemainingBudget();
+    })
+
+    //create a delete button
+    const removeExpenseBtn = document.createElement("button");
+    removeExpenseBtn.innerText = "Delete";
+    expenseSectionItem.appendChild(removeExpenseBtn);
+
+    removeExpenseBtn.addEventListener("click", () => {
+      deleteExpense(expense.id);
+      populateExpenseSection();
+      displayRemainingBudget();
+    });
+
+    // Append the <li> to the parent container
+    expenseItemSection.appendChild(expenseSectionItem);
+    console.log(expenseSectionItem);
+  }); //end of mapping from expensesdata from local storage
+}
+
+function populateIncomeSection() {
+  incomeItemSection.innerText = "";
+  let incomeData = retrieveIncome();
+
+  incomeData.map((income) => {
+    const incomeSectionItem = document.createElement("div"); // favorite item  create
+    incomeSectionItem.classList.add(
+      "flex",
+      "items-center",
+      "p-2",
+      "text-gray-900",
+      "rounded-lg",
+      "dark:text-white",
+      "hover:bg-amber-100",
+      "dark:hover:bg-amber-700",
+      "group"
+    );
+
+    //create another <span> for the amount
+    const incomeAmount = document.createElement("span");
+    incomeAmount.innerText = `${income.amount}`;
+    incomeSectionItem.appendChild(incomeAmount); // Add the amount to the <li>
+
+    //create a delete button
+    const removeIncomeBtn = document.createElement("button");
+    removeIncomeBtn.innerText = "Delete";
+    incomeSectionItem.appendChild(removeIncomeBtn);
+
+    removeIncomeBtn.addEventListener("click", () => {
+      deleteIncome(income.id);
+      populateIncomeSection();
+      displayRemainingBudget();
+    });
+
+    // Append the <li> to the parent container
+    incomeItemSection.appendChild(incomeSectionItem);
+    console.log(incomeItemSection);
+  }); //end of mapping from income data from local storage
+}
+
+function displayRemainingBudget() {
+  remainingBudget.innerText = getBudgetLeft().toFixed(2);
+}
+
+
+addExpenseBtn.addEventListener("click", () => {
+    console.log("EXP VAL"+expenseNameTxt.value);
+    if (expenseNameTxt.value=="" || expenseAmtTxt.value=="")
+        {
+            expenseErrorMsg.innerText="ERROR: expense name and amount are required"
+        }
+    else{
+        addExpense(expenseNameTxt.value, Number(expenseAmtTxt.value));
+        populateExpenseSection();
+        displayRemainingBudget();
+    }
+});
+
+addIncomeBtn.addEventListener("click", () => {
+
+    if (incomeAmtTxt.value=="")
+        {
+            incomeErrorMsg.innerText="ERROR: Income amount is required"
+        }
+    else{
+        addIncome(Number(incomeAmtTxt.value));
+        populateIncomeSection();
+        displayRemainingBudget();
+    }
+
+});
+
+populateIncomeSection();
+populateExpenseSection();
+displayRemainingBudget();
---
+++ scripts/localStorage.js
@@ -0,0 +1,113 @@
+//populate expenses
+// const expenseList = [
+//     { id: 1, name: "Lunch", amount: 10 },
+//     { id: 2, name: "Bus Ticket", amount: 2.5 }
+//   ];
+
+//populate income
+// const incomeList = [
+//     { id: 1, amount: 1000 },
+//     { id: 2, amount: 250.12 }
+//   ];
+
+
+function addExpense(expenseName,amt){
+    let expenseList=retrieveExpenses()
+    console.log(expenseList);
+    console.log(expenseList.length)
+    expenseList.push({id: expenseList.length+1,name :expenseName,amount: amt});
+    console.log(expenseList);
+    localStorage.setItem('Expenses',JSON.stringify(expenseList));
+}
+function deleteExpense(idToDelete){
+    let expenseList=retrieveExpenses()
+    let index =expenseList.findIndex(expense=>expense.id==idToDelete);// find the index of the item wewant to delete
+    console.log("INDEX TO DELETE" +index);
+    expenseList.splice(index, 1); //remove it from the list based on index
+    console.log(expenseList);
+    localStorage.setItem('Expenses', JSON.stringify(expenseList));
+
+ 
+}
+function updateExpense(isToUpdate,newExpenseName, newAmt){
+    let expenseList=retrieveExpenses()
+    let index =expenseList.findIndex(expense=>expense.id==isToUpdate);// find the index of the item wewant to delete
+    console.log("INDEX TO UPDATE" +index);
+    
+    if (index !== -1) { // Check if the item exists
+        expenseList[index].name = newExpenseName; // Update name
+        expenseList[index].amount = Number(newAmt); // Update amount
+        console.log("Updated Expenses:", expenseList);
+    } else {
+        console.log("Expense not found!");
+    }
+    localStorage.setItem('Expenses', JSON.stringify(expenseList));
+
+ 
+}
+
+
+function retrieveExpenses(){
+   
+    let expenseData = localStorage.getItem('Expenses');
+    return JSON.parse(expenseData);
+
+}
+
+function addIncome(amt){
+    let incomeList=retrieveIncome();
+
+    incomeList.push({id:incomeList.length+1,amount:amt});
+    console.log (incomeList);
+    localStorage.setItem('Income',JSON.stringify(incomeList));
+}
+
+function retrieveIncome(){
+let incomeData = localStorage.getItem('Income');
+// console.log("RETRIEVE INCOME"+ JSON.parse(incomeData));
+return JSON.parse(incomeData);
+  
+}
+
+function deleteIncome(idToDelete){
+    let incomeList=retrieveIncome()
+    let index =incomeList.findIndex(income=>income.id==idToDelete);// find the index of the item wewant to delete
+    console.log("INDEX TO DELETE" +index);
+    incomeList.splice(index, 1); //remove it from the list based on index
+    console.log(incomeList);
+    localStorage.setItem('Income', JSON.stringify(incomeList));
+
+ 
+}
+
+
+function getBudgetLeft()
+{
+    let incomeData = retrieveIncome();
+    let totalIncome=0;
+    incomeData.map(income =>{
+        console.log("ID" +income.id);
+        console.log("AMT"+income.amount);
+        totalIncome += income.amount;
+    })
+    console.log("TOTAL"+ totalIncome);
+
+    let expenseData = retrieveExpenses();
+    let totalExpense=0;
+    expenseData.map(expense =>{
+        console.log("ID" +expense.id);
+        console.log("AMT"+expense.amount);
+        totalExpense += expense.amount;
+    })
+    console.log("TOTAL: "+ totalIncome);
+    console.log("EXP: "+totalExpense);
+
+    console.log("BUDGETLEFT" + (totalIncome-totalExpense));
+    return totalIncome-totalExpense;
+}
+
+
+
+
+
+export{addExpense, deleteExpense,updateExpense,retrieveExpenses,addIncome,deleteIncome,retrieveIncome,getBudgetLeft}
